362fd543e033eb8cc0313c30cbbb976fceb48c76
azuredev24 <stefancurti31@gmail.com>
2024-08-31 18:59:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿Console.WriteLine("hello word");
+int a = 12;
+int b = 13;
+int c = a + b;
+const double x = 23.8;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(x);
+
+double d = 12.6;
+Console.WriteLine(d);
+
+String e = "Stefan";
+Console.WriteLine(e);
+
+Boolean elev = true;
+Console.WriteLine( "elevul este prezent in clasa ? " + elev);
+
+char symbol = '@';
+Console.WriteLine(symbol);
+
+int f = 9;
+Double g = Convert.ToDouble(f);
+Console.WriteLine(g.GetType());
+
+double h = 12.8;
+int i = Convert.ToInt32(h);
+Console.WriteLine(i.GetType());
+
+String j = "#";
+char k = Convert.ToChar(j);
+Console.WriteLine(k.GetType());
+
+Boolean q = true;
+string w = Convert.ToString(q);
+Console.WriteLine(w.GetType());
+
+String p = "true";
+Boolean z = Convert.ToBoolean(p);
+Console.WriteLine(z.GetType());
+
+Console.WriteLine("Cum te cheama ? ");
+Console.ReadLine();
+
+Console.WriteLine("Cati ani ai ? ");
+Console.ReadLine();
+
+
+int friends = 8;
+//friends = friends + friends;
+friends++;
+//friends += 2;
+Console.WriteLine(friends);
+
+
+
+
+
+
